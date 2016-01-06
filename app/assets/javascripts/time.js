b7c8511a35e97f4e7838edb7f01c53967a257f34
adamgoodapp <adamgoodapp@gmail.com>
2016-01-06 01:10:42 +0000

    Add back plain tine

--- app/assets/javascripts/time.js
+++ app/assets/javascripts/time.js
@@ -1,45 +1,43 @@
-var months = [
-  "JAN", "FED", "MAR", "APR",
-  "MAY", "JUN", "JUL", "AUG",
-  "SEPT", "OCT", "NOV", "DEC"
-];
-
-(function() {
-  var getTime = function() {
-    var now = new Date();
-    return {
-      seconds: ('0' + now.getSeconds()).slice(-2),
-      minutes: ('0' + now.getMinutes()).slice(-2),
-      hours: now.getHours(),
-      day: now.getDate(),
-      month: now.getMonth(),
-      year: now.getFullYear()
-    };
-  }
-  var updateTime = function(time) {
-    var hourBar = document.getElementById("hour");
-    var hourRatio = (time.hours / 24);
-    var minutesBar = document.getElementById("minutes");
-    var minutesRatio = (time.minutes / 60);
-    var secondsBar = document.getElementById("seconds");
-    var secondsRatio = (time.seconds / 60);
-    document.getElementById("date").innerHTML =
-        months[time.month] + " " +
-        time.day + ", " + time.year +
-        " " + time.hours + ":" +
-        time.minutes + ":" + time.seconds;
-    hourBar.style.width = hourRatio * 100 + "%";
-    //hourBar.style.backgroundColor = "hsl(" + ((1 - hourRatio) * 100) + ",50%,50%)";
-    hourBar.style.backgroundColor = "#fff";
-    minutesBar.style.width = minutesRatio * 100 + "%";
-    //minutesBar.style.backgroundColor = "hsl(" + ((1 - minutesRatio) * 100) + ",50%,50%)";
-    minutesBar.style.backgroundColor = "#fff";
-    secondsBar.style.width = secondsRatio * 100 + "%";
-    //secondsBar.style.backgroundColor = "hsl(" + ((1 - secondsRatio) * 100) + ",50%,50%)";
-    secondsBar.style.backgroundColor = "#fff";
+$( document ).ready(function() {
+  function ordinal_suffix_of(i) {
+    var j = i % 10,
+        k = i % 100;
+    if (j == 1 && k != 11) {
+      return i + "st";
+    }
+    if (j == 2 && k != 12) {
+      return i + "nd";
+    }
+    if (j == 3 && k != 13) {
+      return i + "rd";
+    }
+    return i + "th";
   }
 
   setInterval(function() {
-    return updateTime(getTime());
+    var monthNames = ["January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+    ];
+
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    var date = ordinal_suffix_of(currentTime.getDate()) + " " + monthNames[currentTime.getMonth()] + " " + currentTime.getFullYear();
+
+    if (minutes < 10){
+      minutes = "0" + minutes
+    }
+    var t_str = hours + ":" + minutes + ":" + seconds;
+    var date_str = date;
+
+    if(hours > 11){
+      t_str += " PM";
+    } else {
+      t_str += " AM";
+    }
+
+    $('.date').html(date_str);
+    $('.time').html(t_str);
   }, 1000);
-})();
+});
